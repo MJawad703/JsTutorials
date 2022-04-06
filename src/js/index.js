4b2bba24caac66ee859ef928f1c8f975a6e6918e
MJawad703 <m.jawad3300@gmail.com>
2022-04-06 20:23:13 +0500

    Arrow functions and adding and removing all classes

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ const add = document.querySelector("#add");
 const remove = document.querySelector("#remove");
 const check = document.querySelector("#check");
 const addAll = document.querySelector("#addAll");
+const toggle = document.querySelector("#toggle");
 const removeAll = document.querySelector("#removeAll");
 const paragraph = document.querySelector("#paragraph");
 
@@ -10,6 +11,7 @@ add.addEventListener("click", function () {
 });
 
 remove.addEventListener("click", function () {
+  // paragraph.setAttribute("styles", "");
   paragraph.classList.remove("styles");
 });
 
@@ -18,3 +20,32 @@ check.addEventListener("click", function () {
   // console.log(isPresent);
   paragraph.innerHTML = isPresent;
 });
+
+toggle.addEventListener("click", function () {
+  paragraph.classList.toggle("styles");
+});
+
+addAll.addEventListener("click", function () {
+  paragraph.classList.add("styles", "paragraph", "abc");
+});
+
+removeAll.addEventListener("click", function () {
+  const classes = paragraph.classList;
+  paragraph.classList.remove("styles", "paragraph", "abc");
+});
+
+// Arrow Function
+
+function addNumber(a, b) {
+  return a + b;
+}
+
+const addNumberArrow = (a, b) => a + b;
+
+console.log(addNumber(12, 12));
+console.log(addNumberArrow(12, 12));
+
+const person = {
+  population: "123",
+  country: "string",
+};
