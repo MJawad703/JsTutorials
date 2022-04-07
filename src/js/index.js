fdc1ab2eb187f8476289f12d368a027601230eb1
MJawad703 <m.jawad3300@gmail.com>
2022-04-07 20:28:20 +0500

    inline Js styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,14 @@
-const add = document.querySelector("#add");
-const remove = document.querySelector("#remove");
-const check = document.querySelector("#check");
-const addAll = document.querySelector("#addAll");
-const toggle = document.querySelector("#toggle");
-const removeAll = document.querySelector("#removeAll");
-const paragraph = document.querySelector("#paragraph");
+const box = document.getElementById("box");
+const btn = document.getElementById("btn");
+let isLogin = true;
 
-add.addEventListener("click", function () {
-  paragraph.classList.add("styles");
-});
+box.style.width = "100px";
+box.style.height = "100px";
+box.style.backgroundColor = "red";
+box.style.margin = "40px";
 
-remove.addEventListener("click", function () {
-  // paragraph.setAttribute("styles", "");
-  paragraph.classList.remove("styles");
-});
-
-check.addEventListener("click", function () {
-  const isPresent = paragraph.classList.contains("styles");
-  // console.log(isPresent);
-  paragraph.innerHTML = isPresent;
-});
-
-toggle.addEventListener("click", function () {
-  paragraph.classList.toggle("styles");
-});
-
-addAll.addEventListener("click", function () {
-  paragraph.classList.add("styles", "paragraph", "abc");
-});
-
-removeAll.addEventListener("click", function () {
-  const classes = paragraph.classList;
-  paragraph.classList.remove("styles", "paragraph", "abc");
-});
-
-// Arrow Function
-
-function addNumber(a, b) {
-  return a + b;
+if (isLogin) {
+  box.style.display = "block";
+} else {
+  box.style.display = "none";
 }
-
-const addNumberArrow = (a, b) => a + b;
-
-console.log(addNumber(12, 12));
-console.log(addNumberArrow(12, 12));
-
-const person = {
-  population: "123",
-  country: "string",
-};
