da8f2aacb0b99b70ce9d5245047ec4d292138ea2
MJawad703 <m.jawad3300@gmail.com>
2022-04-15 20:24:38 +0500

    Created Replace and Find Plain Method and discussed about split and join

--- src/js/functions.js
+++
@@ -1,8 +0,0 @@
-export const checkClass = (number, element) => {
-  if (number >= 0) {
-    element.classList.add("success");
-  } else {
-    element.classList.remove("success");
-    element.classList.add("danger");
-  }
-};
--- src/js/index.js
+++ src/js/index.js
@@ -7,14 +7,29 @@ window.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("button");
   button.addEventListener("click", (event) => {
     event.preventDefault();
-    const findValue = find.value;
-    const replaceValue = replace.value;
-    const textareaValue = textarea.value;
-    while (textareaValue.indexOf(findValue) != -1) {
-      textarea.value = textareaValue.replace(findValue, replaceValue);
+    let findValue = find.value;
+    let replaceValue = replace.value;
+    // Converting textarea.value into an Array
+    let textareaValue = textarea.value.split(" ");
+
+    for (let a = 0; a <= textareaValue.length; a++) {
+      if (textareaValue[a] === findValue) {
+        textareaValue[a] = replaceValue;
+      }
     }
+
+    // Converting textareaValue into a string from an array.
+    textareaValue = textareaValue.join(" ");
+    // console.log(textareaValue);
+    textarea.value = textareaValue;
+    // while (textarea.value.indexOf(find.value) != -1) {
+    //   textarea.value = textarea.value.replace(find.value, replace.value);
+    // }
+    // console.log(newText);
   });
 });
 
-// let str = "I am Jawad";
-// console.log(str.indexOf("Jawad"));
+// let str = "I am Jawad Jawad";
+//if it finds a value then that value will be greater than -1
+// if it doesn't find value then that will be -1.
+// console.log(str.replace("Jawad", "Suhaib"));
