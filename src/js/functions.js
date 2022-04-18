75ab38bf32ccaf5b16d1d7c4d4d9adeee5fe40b7
MJawad703 <m.jawad3300@gmail.com>
2022-04-18 20:15:13 +0500

    Arrow Functions and Default exports

---
+++ src/js/functions.js
@@ -0,0 +1,5 @@
+const abc = () => {
+  console.log("Hello");
+};
+
+export default abc;
--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,20 @@
-// DOMContentLoaded;
+// Default exports
+import israr from "./functions.js";
 
-const searchReplace = (find, replace, textarea) => {
-  let findValue = find.value.toLowerCase();
-  let replaceValue = replace.value;
-  // Converting textarea.value into an Array
-  let textareaValue = textarea.value.toLowerCase().split(" ");
+function sum(a, b) {
+  return a + b;
+}
+israr();
+// Arrow Function
 
-  for (let a = 0; a <= textareaValue.length; a++) {
-    if (textareaValue[a] === findValue) {
-      textareaValue[a] = replaceValue;
-    }
-  }
+const sumValue = (a, b) => a + b;
 
-  // Converting textareaValue into a string from an array.
-  textareaValue = textareaValue.join(" ");
-  // console.log(textareaValue);
-  textarea.style.textTransform = "capitalize";
-  textarea.value = textareaValue;
-  // while (textarea.value.indexOf(find.value) != -1) {
-  //   textarea.value = textarea.value.replace(find.value, replace.value);
-  // }
-  // console.log(newText);
-};
+function multiplyByTwo(a) {
+  return a * 2;
+}
 
-window.addEventListener("DOMContentLoaded", () => {
-  const find = document.getElementById("find");
-  const replace = document.getElementById("replace");
-  const textarea = document.getElementById("textarea");
-  const button = document.getElementById("button");
-  button.addEventListener("click", (event) => {
-    event.preventDefault();
-    searchReplace(find, replace, textarea);
-  });
-});
+// Arrow Function
 
-// let str = "I am Jawad Jawad";
-//if it finds a value then that value will be greater than -1
-// if it doesn't find value then that will be -1.
-// console.log(str.replace("Jawad", "Suhaib"))
+const arrowMultiplyByTwo = (a) => a * 2;
+
+// Changes other than syntax
