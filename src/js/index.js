46de534319b3174be91cfe89a7830c70d40be369
Muhammad Jawad <m.jawad3300@gmail.com>
2022-05-17 10:13:36 -0700

    Map method of JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,73 @@
-const array = [2, 3, 5, 1];
+// const array = [2, 3, 5, 1];
 
-// Push and Pop
-array.push("new element");
-console.log(array);
-array.pop();
-console.log(array);
-
-// includes
-console.log(array.includes(10));
-
-// indexOf
-console.log(array.indexOf(5));
-
-// sort
-// array.sort();
+// // Push and Pop
+// array.push("new element");
+// console.log(array);
+// array.pop();
 // console.log(array);
 
-// slice
-// console.log(array.slice(0, 3));
+// // includes
+// console.log(array.includes(10));
 
-// reverse
-console.log(array.reverse());
+// // indexOf
+// console.log(array.indexOf(5));
 
-// Map
+// // sort
+// // array.sort();
+// // console.log(array);
 
-// Filter
+// // slice
+// // console.log(array.slice(0, 3));
 
-// Reduce
+// // reverse
+// console.log(array.reverse());
 
-// Some
+// // Map
+let blankArray = [];
+let numbersArray = [1, 2, 3, 4, 5];
+// let mappedArray = numbersArray.map((item, index) => ({
+//   id: index,
+//   value: item,
+// }));
 
-// Every
+function multiply(arr) {
+  return arr * 2;
+}
+const mappedArray = numbersArray.map((item) => multiply(item));
+console.log(mappedArray);
+
+const data = [
+  { id: 1, name: "Jawad" },
+  { id: 2, name: "Israr" },
+  { id: 3, name: "Suhaib" },
+  { id: 4, name: "Burhan " },
+  { id: 5, name: "Arsalan" },
+  { id: 6, name: "Mohib" },
+];
+
+const newMappedArray = data.map((item, index) => ({
+  name: item.name,
+  serviceId: index + item.id,
+}));
+
+console.log(newMappedArray);
+
+// const boxes = document.querySelectorAll(".box");
+// boxes.forEach((item, index) => {
+//   const newBox = document.createElement("h1");
+//   newBox.innerText = index;
+//   item.appendChild(newBox);
+// });
+
+// console.log(boxes);
+
+// console.log(mappedArray);
+// it takes an array an generate a new Array
+
+// // Filter
+
+// // Reduce
+
+// // Some
+
+// // Every
